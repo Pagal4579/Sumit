c9c14bb60540cff2a75277db5ad7e4e8f4592ec6
Kriti970 <kritikori209@gmail.com>
2024-08-31 11:06:41 +0530

    Update 0_AADI-HARTLESS.js

--- Priyansh/commands/0_AADI-HARTLESS.js
+++ Priyansh/commands/0_AADI-HARTLESS.js
@@ -20,7 +20,7 @@ react.includes("Masoom girl") ||
 react.includes("masoom")) {
     var msg = {
         body: `🥰 𝐇𝐀𝐑𝐓𝐋𝐄𝐒𝐒 𝐐𝐔𝐄𝐄𝐍 🥰
-    𝐃𝐔𝐑 𝐇𝐀𝐓 𝐓𝐄𝐑𝐄𝐊𝐎 𝐊𝐎𝐈 𝐀𝐔𝐑 𝐊𝐀𝐌 𝐍𝐀𝐇𝐈 𝐉𝐀𝐁 𝐃𝐀𝐊𝐇𝐎 𝐇𝐀𝐑𝐓𝐋𝐄𝐒𝐒 𝐇𝐀𝐑𝐓𝐋𝐄𝐒𝐒 𝐊𝐀𝐑𝐓𝐄 𝐑𝐄𝐇𝐓𝐄 𝐇𝐎 😝😝`,attachment: fs.createReadStream(__dirname + `/noprefix/babua.jpeg`)
+    𝐃𝐔𝐑 𝐇𝐀𝐓 𝐓𝐄𝐑𝐄𝐊𝐎 𝐊𝐎𝐈 𝐀𝐔𝐑 𝐊𝐀𝐌 𝐍𝐀𝐇𝐈 𝐉𝐀𝐁 𝐃𝐀𝐊𝐇𝐎 𝐇𝐀𝐑𝐓𝐋𝐄𝐒𝐒 𝐇𝐀𝐑𝐓𝐋𝐄𝐒𝐒 𝐊𝐀𝐑𝐓𝐄 𝐑𝐄𝐇𝐓𝐄 𝐇𝐎 😝😝`,attachment: fs.createReadStream(__dirname + `/noprefix/download (5) (1).mp4`)
       }
       api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("😘", event.messageID, (err) => {}, true)
