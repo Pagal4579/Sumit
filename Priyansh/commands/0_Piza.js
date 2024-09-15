647c80d0d1ea0b0596b06525ad720146ddc605a7
Kriti970 <kritikori209@gmail.com>
2024-09-15 17:33:08 +0530

    Update and rename amogus.js to 0_Piza.js

---
+++ Priyansh/commands/0_Piza.js
@@ -0,0 +1,31 @@
+//@Prem-babu3608
+////////////////////////////////////////////////////////
+/////// WARNING => JO CREDIT NAME CHANGE KREGA USKA ID BAN KAR DIYA JAYEGA + THIS BOT IS MADE BT PREM BABU
+const fs = require("fs");
+module.exports.config = {
+	name: "piza",
+    version: "1.1.1",
+	hasPermssion: 0,
+	credits: "PREM BABU", //////@prem-babu3608
+	description: "THIS BOT IS MR PREM SHARMA",
+	commandCategory: "no prefix",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	let react = event.body.toLowerCase();
+	if(react.includes("⭐") ||
+     react.includes("piza") || react.includes("🫓") || react.includes("खाना") ||
+react.includes("🌔") ||
+react.includes("🦕")) {
+		var msg = {
+				body: `Jaldi Jaldi kha lo Koi aa jayga😂😜`,attachment: fs.createReadStream(__dirname + `/AADI/piz.jpg`)
+			}
+			api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("🍲", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+	}
