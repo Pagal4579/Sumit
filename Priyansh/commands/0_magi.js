82831e19657e09ffa778b6653b6f47835d88f3e0
Kriti970 <kritikori209@gmail.com>
2024-09-15 18:33:07 +0530

    Update 0_magi.js

--- Priyansh/commands/0_magi.js
+++ Priyansh/commands/0_magi.js
@@ -15,7 +15,7 @@ module.exports.config = {
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	let react = event.body.toLowerCase();
-	if(react.includes("Magi") ||
+	if(react.includes("megi") ||
      react.includes("🛸") || react.includes("🏟️") || react.includes("🦦") ||
 react.includes("🗿") ||
 react.includes("🗻")) {
