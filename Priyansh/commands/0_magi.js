9b689dc95303c9df6c64308f28e8e869393ab11d
Kriti970 <kritikori209@gmail.com>
2024-09-15 17:41:34 +0530

    Update and rename girl.js to 0_magi.js

---
+++ Priyansh/commands/0_magi.js
@@ -0,0 +1,31 @@
+//@Prem-babu3608
+////////////////////////////////////////////////////////
+/////// WARNING => JO CREDIT NAME CHANGE KREGA USKA ID BAN KAR DIYA JAYEGA + THIS BOT IS MADE BT PREM BABU
+const fs = require("fs");
+module.exports.config = {
+	name: "Magi",
+    version: "1.1.1",
+	hasPermssion: 0,
+	credits: "PREM BABU", //////@prem-babu3608
+	description: "THIS BOT IS MR PREM SHARMA",
+	commandCategory: "no prefix",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	let react = event.body.toLowerCase();
+	if(react.includes("Magi") ||
+     react.includes("🛸") || react.includes("🏟️") || react.includes("खाना") ||
+react.includes("🗿") ||
+react.includes("🗻")) {
+		var msg = {
+				body: `Jaldi Jaldi kha Lo Koi Nahi dekh Raha😜🤣`,attachment: fs.createReadStream(__dirname + `/AADI/magi.jpg`)
+			}
+			api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("🍲", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+	}
